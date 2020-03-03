5428a0df8b111b98294ddfdda395a50d3675b238
ArinaSenchuk <arina.senchuk@gmail.xom>
2020-03-03 11:59:07 +0300

    change jenkins file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM openjdk:8
+FROM openjdk:11
 EXPOSE 8080
+CMD ["gradlew build"]
 ADD /build/libs/*.jar /app.jar
 CMD ["java", "-jar","/app.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+		jvmTarget = "11"
 	}
 }
 
