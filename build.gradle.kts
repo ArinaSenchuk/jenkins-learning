3aab9ae8ec8549602525c7a1b40303986022bbe6
ArinaSenchuk <arina.senchuk@gmail.xom>
2020-03-03 12:09:15 +0300

    change jenkins file

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,13 +15,13 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 repositories {
 	mavenCentral()
 }
-
-extra["vaadinVersion"] = "14.1.17"
+//
+//extra["vaadinVersion"] = "14.1.17"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("com.vaadin:vaadin-spring-boot-starter")
+//	implementation("com.vaadin:vaadin-spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
@@ -31,11 +31,11 @@ dependencies {
 	}
 }
 
-dependencyManagement {
-	imports {
-		mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
-	}
-}
+//dependencyManagement {
+//	imports {
+//		mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
+//	}
+//}
 
 tasks.withType<Test> {
 	useJUnitPlatform()
@@ -44,7 +44,7 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+		jvmTarget = "1.8"
 	}
 }
 
